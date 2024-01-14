5523dc47be99e2da982f545afcde17f3a5e1f335
coderirshad <irshadkhanofficial46@gmail.com>
2024-01-14 20:28:18 +0530

    file multer is given

--- models/user.js
+++ models/user.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const multer = require('multer');
+const path = require('path');
+const AVATAR_PATH = path.join('/uploads', 'users', 'avatars');
+console.log(AVATAR_PATH);
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -13,10 +17,26 @@ const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
-    }
+    },
+    // avatar:{
+    //     type:string
+    // }
 }, {
     timestamps: true
 });
+
+//configure multer
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname,'..',AVATAR_PATH));
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+  });
+  userSchema.statics.uploadedAvatars = multer({ storage: storage }).single('avatar');
+  userSchema.statics.avatarPath = AVATAR_PATH;
 
 
 const User = mongoose.model('User', userSchema);
