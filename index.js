54f02a3c91c104876b2d36ec2b7050f67fa6d11c
coderirshad <irshadkhanofficial46@gmail.com>
2024-01-15 21:26:35 +0530

    google authorization is added

--- index.js
+++ index.js
@@ -9,27 +9,28 @@ const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const passportJWT = require('./config/passport-jwt-strategy');
+const passportGoogle = require('./config/passport-google-oauth2-strategy');
+
 const MongoStore = require('connect-mongo')(session);
-const sassMiddleware =require('node-sass-middleware');
+const sassMiddleware = require('node-sass-middleware');
 const flash = require('connect-flash');
-const customMware = require('./config/middleware')
+const customMware = require('./config/middleware');
+
 
 app.use(sassMiddleware({
-    src: './assets/scss ',
+    src: './assets/scss',
     dest: './assets/css',
     debug: true,
     outputStyle: 'extended',
     prefix: '/css'
 }));
-
-
-
 app.use(express.urlencoded());
 
 app.use(cookieParser());
 
 app.use(express.static('./assets'));
-app.use('/uploads',express.static(__dirname + '/uploads'))
+// make the uploads path available to the browser
+app.use('/uploads', express.static(__dirname + '/uploads'));
 
 app.use(expressLayouts);
 // extract style and scripts from sub pages into the layout
@@ -43,7 +44,7 @@ app.set('layout extractScripts', true);
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-// // mongo store is used to store the session cookie in the db
+// mongo store is used to store the session cookie in the db
 app.use(session({
     name: 'codeial',
     // TODO change the secret before deployment in production mode
@@ -63,13 +64,13 @@ app.use(session({
             console.log(err ||  'connect-mongodb setup ok');
         }
     )
-    
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(passport.setAuthenticatedUser);
+
 app.use(flash());
 app.use(customMware.setFlash);
 
